32d1122d4bac0ca83f899bc828885f95ee2d531d
Filip <filipek14204@gmail.com>
2025-05-28 22:39:39 +0200

    partially working play mode with sphere

--- sequence.py
+++ sequence.py
@@ -7,13 +7,22 @@ class SequenceManager:
         self.play_index = 0
 
     def record_frame(self):
-        self.frames.append(copy.deepcopy(self.robot.joint_angles))
-
+        self.frames.append({
+        'angles': copy.deepcopy(self.robot.joint_angles),
+        'grabbing': self.robot.grabbing,
+        'grabbed_object': self.robot.grabbed_object  # tu tylko referencja, nie deep copy!
+    })
     def playback(self):
         if not self.frames:
             return
         if self.play_index >= len(self.frames):
             self.play_index = 0
-        self.robot.joint_angles = copy.deepcopy(self.frames[self.play_index])
+
+        frame = self.frames[self.play_index]
+        self.robot.joint_angles = copy.deepcopy(frame['angles'])
+        self.robot.grabbing = frame['grabbing']
+        self.robot.grabbed_object = frame['grabbed_object']
         self.play_index += 1
-        
+
+        if self.robot.grabbing and self.robot.grabbed_object:
+            self.robot.grabbed_object.position = self.robot.get_end_effector_pos()
